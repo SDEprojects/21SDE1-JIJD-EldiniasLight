49ea0f38d6b806b95d94a821b8a8daaab2bedfa5
Ibrahim Gurhan <ibrahimgurhan.dev@gmail.com>
2021-05-13 03:00:25 -0400

    connected GUI and game engine together,AB#534

--- src/com/fourforfour/eldanialight/UIFrame/UI.java
+++ src/com/fourforfour/eldanialight/UIFrame/UI.java
@@ -1,170 +1,143 @@
 package com.fourforfour.eldanialight.UIFrame;
 
-import com.fourforfour.eldanialight.SimpleAudioPlayer;
+import com.fourforfour.eldanialight.controller.SwingController;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Locale;
 
 public class UI extends JFrame {
 
-    //This method creates the frame to begin the game
-    private JFrame frame, locationOneScreen; //Creates a frame
-    private JTextArea textArea, locationInfo;
-    private BufferedImage image;
-    private JLabel titleLabel, commandInputLabel, locationImageContainer, commandLabel;
+    private SwingController controller;
+
+    private final int FRAME_X_SIZE = 520;
+    private final int FRAME_Y_SIZE = 900;
+
+    private JTextArea displayArea;
     private JTextField playerInputTF;
-    private JPanel titleNamePanel, startButtonPanel, exitButtonPanel;
-    private JButton startButton, exitButton;
+    private JLabel saveGameMsgLbl;
+    private JTextArea playerMessageLbl;
+    private JButton submitCommandBtn;
 
-    HandleSubmitButtonWhenClicked submitHandle = new HandleSubmitButtonWhenClicked();
+    private JLabel locationImageContainer;
 
-    UI() {
-        //Setting up the title of the game
-        titleLabel = new JLabel("Eldinias Light"); //create a label and set text of label
-        titleLabel.setBounds(240, 0, 200, 50);
-        titleLabel.setFont(new Font("Book Antiqua", Font.BOLD, 20));//sets font of the text
 
-        commandLabel = new JLabel("Command Label"); //create a label and set text of label
-        commandLabel.setForeground(Color.RED);
-        commandLabel.setBounds(240, 525, 100, 100);
-        commandLabel.setFont(new Font("Book Antiqua", Font.BOLD, 10));//sets font of the text
 
-        //Setting up the location image
+
+    public UI(SwingController controller) {
+        this.controller = controller;
+
+        setLocation(100,100);
+        setSize(FRAME_X_SIZE,FRAME_Y_SIZE);
+        setResizable(false);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         Image locationImage = null;
         try {
-            locationImage = ImageIO.read(new File("Eldinias-Light/data/images/Opening screen.jpg"));
+            locationImage = ImageIO.read(new File("data/images/Opening screen.jpg"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        locationImage = locationImage.getScaledInstance(580, 300, Image.SCALE_SMOOTH);
-        locationImageContainer = new JLabel();
-        locationImageContainer.setIcon(new ImageIcon(locationImage));
-        locationImageContainer.setBounds(10, 50, 580, 300);
 
-        //setting up location info label
-        locationInfo = new JTextArea(25, 40);
-        locationInfo.setBounds(10, 360, 580, 120);
-        locationInfo.setEditable(false);
-        locationInfo.setLineWrap(true); //fit all the words in the given area
-        locationInfo.setWrapStyleWord(true);
-        locationInfo.setText("Need to grab the info from the JSON.");
-        locationInfo.setFont(new Font("Book Antiqua", Font.PLAIN, 15));//sets font of the text
-        locationInfo.setBackground(Color.orange);
+        locationImageContainer = new JLabel(new ImageIcon(locationImage), SwingConstants.CENTER);
+        locationImageContainer.setBounds(0,50,500,260);
+        add(locationImageContainer);
 
-        //Setting up the command input label
-        commandInputLabel = new JLabel("Command Input:"); //create a label and set text of label
-        commandInputLabel.setBounds(50, 567, 200, 50);
-        commandInputLabel.setFont(new Font("Book Antiqua", Font.BOLD, 12));//sets font of the text
-        commandInputLabel.setForeground(Color.ORANGE);
 
-        //Setting up the player input box
+        displayArea = new JTextArea(25,40);
+        displayArea.setBounds(25,315,430,250);
+        displayArea.setEditable(false);
+        displayArea.setLineWrap(true);
+        displayArea.setWrapStyleWord(true);
+        displayArea.setText("The once peaceful land of Eldina has recently been taken over by the evil warlock Tyroneious the Black who has cast a darkness upon the land. You are the one chosen to defeat Tyroneious and his Army to restore Eldina back to its peaceful ways.");
+        displayArea.setBackground(Color.orange);
+        displayArea.setFont(new Font("Book Antiqua", Font.PLAIN, 15));//sets font of the text
+        add(displayArea);
+
+
+        playerMessageLbl = new JTextArea(15,40);
+        playerMessageLbl.setBounds(25,570,430,120);
+        playerMessageLbl.setEditable(false);
+        playerMessageLbl.append("Welcome Champion and thank you for coming to aid of Eldinia!!");
+        playerMessageLbl.append("\nTell us about this brave soul who has come to defeat Tyronious the Black\n");
+        playerMessageLbl.append("\nWhat type of player are you");
+        playerMessageLbl.append("\nChoose from Mage or Knight or Archer");
+        playerMessageLbl.setWrapStyleWord(true);
+        playerMessageLbl.setLineWrap(true);
+        playerMessageLbl.setFont(new Font("Book Antiqua", Font.PLAIN, 15));//sets font of the text
+        add(playerMessageLbl);
+
+
+//        saveGameMsgLbl = new JLabel("use save/load commands to save/load game", SwingConstants.LEFT);
+//        saveGameMsgLbl.setBounds(25,780,430,25);
+//        JLabel label = new JLabel("I'm bold");
+//        Font font = new Font("Courier", Font.BOLD,12);
+//        saveGameMsgLbl.setFont(font);
+//        Font f = label.getFont();
+//        saveGameMsgLbl.setFont(f.deriveFont(f.getStyle() & ~Font.BOLD));
+//        add(saveGameMsgLbl);
+
         playerInputTF = new JTextField();
-        playerInputTF.setBounds(46, 580, 500, 80);
+        playerInputTF.setBounds(25,695,430,25);
+        playerInputTF.addActionListener(new HandleSubmitButtonPressEnter());
         playerInputTF.setBackground(Color.BLACK);
         playerInputTF.setForeground(Color.ORANGE);
         playerInputTF.setFont(new Font("Book Antiqua", Font.PLAIN, 20));//sets font of the text
-        playerInputTF.setHorizontalAlignment(JTextField.CENTER); //sets the start point to center
-        playerInputTF.addActionListener(new HandleSubmitButtonWhenClicked());
-        playerInputTF.addActionListener(new HandleSubmitButtonPressEnter());
+        playerInputTF.setHorizontalAlignment(JTextField.CENTER);
+        add(playerInputTF);
 
-        //Setting up the frame
-        frame = new JFrame("Crypto Millionaire Presents");
-        frame.setLocation(100, 100);
-        frame.setLayout(null);
-        frame.setSize(600, 700);//sets height/width of the frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exits out of application
-        frame.setResizable(false);//user cannot resize frame
-        frame.getContentPane().setBackground(Color.white);//sets frame background to the color of choice
+        submitCommandBtn = new JButton();
+        submitCommandBtn.setBounds(380,725,75,25);
+        submitCommandBtn.setText("Submit");
+        submitCommandBtn.addActionListener(new HandleSubmitButtonWhenClicked());
+        add(submitCommandBtn);
 
-        startButton = new JButton("Submit");
-        startButton.setBounds(545, 600, 50, 20);
-        startButton.addActionListener(new HandleSubmitButtonWhenClicked());
-        startButton.addActionListener(new HandleSubmitButtonWhenClicked());
-        startButton.setBackground(Color.white);
-        startButton.setForeground(Color.black);
-
-
-        frame.add(startButton);
-        frame.add(titleLabel);
-        frame.add(commandLabel);
-        frame.add(commandInputLabel);
-        frame.add(playerInputTF);
-        frame.add(locationInfo);
-        frame.add(locationImageContainer);
-        frame.setVisible(true);//makes the frame visible
-
-
-    }
-
-    public void displayLocationOne() {
-        //closes displayOpenScreen panel when called
-        locationOneScreen = new JFrame("Crypto Millionaire Presents");
-        titleNamePanel.setVisible(false);
-        startButtonPanel.setVisible(false);
-        frame.setVisible(false);
-
-        //Setting up the frame
-        locationOneScreen.setVisible(true); //makes the frame visible
-        locationOneScreen.setSize(1000, 600);//sets height/width of the frame
-        locationOneScreen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//exits out of application
-        locationOneScreen.setResizable(false);//user cannot resize frame
-        locationOneScreen.getContentPane().setBackground(Color.white);//sets frame background to the color of choice
-        locationOneScreen.setLayout(null);//set text is a certain position
-
-        //setting up the test area inside the frame
-
-        textArea = new JTextArea("This is working.");//example text to see where the string shows u[
-        textArea.setBounds(50, 410, 700, 150);//sets the dimensions of the text box
-        textArea.setBackground(Color.black);//sets the box to the color of choice
-        textArea.setForeground(Color.white);//sets the words in the text box to color of choice
-        textArea.setLineWrap(true);//a multi-line area that displays plain text
-        textArea.setWrapStyleWord(true);// for your text is wrapped by words and not by the characters
-        textArea.setFont(new Font("Book Antiqua", Font.PLAIN, 26));//sets font of the text
-        locationOneScreen.add(textArea);//makes the text appear
     }
 
     private void processSubmitInput(){
-        //String inputText = playerInputTF.getText().toLowerCase();
-        //String result = controller....
-        System.out.println("hey");
+        String inputText = playerInputTF.getText().toLowerCase();
+        String result = controller.processInput(inputText);
+        playerMessageLbl.setText(result);
+        String displayData = controller.getDescription();
+        displayArea.setText(displayData);
+
+        playerInputTF.setText("");
 
     }
 
-    //Transitions between the open screen and first location screen
     public class HandleSubmitButtonWhenClicked implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println("hey");
+            processSubmitInput();
         }
     }
 
     public class HandleSubmitButtonPressEnter implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println(playerInputTF.getText());
+            processSubmitInput();
         }
     }
 
-    public static void main(String[] args) {
-        UI ui = new UI();
-        //used to play the audio wav file when the game is running
-//        Thread thread = new Thread(() -> {
-//            SimpleAudioPlayer audioPlayer;
-//            try {
-//                String filePath = "Eldinias-Light/src/com/fourforfour/eldanialight/UIFrame/01-prelude game opener.wav";
-//                audioPlayer = new SimpleAudioPlayer(filePath);
-//                audioPlayer.play();
-//            } catch (Exception e) {
-//                System.out.println(e.getMessage());
-//            }
-//        });
-//        thread.start();
+//    public static void main(String[] args) {
+//        UI ui = new UI();
+//        //used to play the audio wav file when the game is running
+////        Thread thread = new Thread(() -> {
+////            SimpleAudioPlayer audioPlayer;
+////            try {
+////                String filePath = "Eldinias-Light/src/com/fourforfour/eldanialight/UIFrame/01-prelude game opener.wav";
+////                audioPlayer = new SimpleAudioPlayer(filePath);
+////                audioPlayer.play();
+////            } catch (Exception e) {
+////                System.out.println(e.getMessage());
+////            }
+////        });
+////        thread.start();
+////    }
 //    }
-    }
 }
